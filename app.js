1636f6401fd4011453fbbf5c21cec88546497a82
ThandokuhleNdlebe <thandotydx@gmail.com>
2025-02-19 11:15:36 +0200

    app.js and package.json edited

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const PORT = 5000 || process.env.port;
+
+app.get('', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, ()=> {
+console.log(` App listening on port ${PORT}`);
+});
