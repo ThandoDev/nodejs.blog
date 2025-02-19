34aa3f6472139d9f04f960eead36f182c2ffedd0
ThandokuhleNdlebe <thandotydx@gmail.com>
2025-02-19 12:52:57 +0200

    meta,description + name added

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Routes
 router.get('', (req, res) => {
-    res.render('index');
+const locals = {
+    title: "nodejs blog",
+    description: "Simple blog created with NodeJS, Express & MongoDb."
+}
+
+
+    res.render('index', { locals } );
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 
