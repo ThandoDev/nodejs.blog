c1f6927c1d7bb537658c7b5ea5bd2ed557d8e7aa
ThandokuhleNdlebe <thandotydx@gmail.com>
2025-02-19 12:12:28 +0200

    home page added

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const expressLayout = require('express-ejs-layouts');
 const app = express();
 const PORT = 5000 || process.env.port;
 
-app.get('', (req, res) => {
-    res.send("Hello World");
-});
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main')); 
 
 app.listen(PORT, ()=> {
-console.log(` App listening on port ${PORT}`);
+console.log(`App listening on port ${PORT}`);
 });
